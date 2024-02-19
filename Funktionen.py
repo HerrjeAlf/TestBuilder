9fc2d6cc8b4685b424e94c9505f37001cd78b838
HerrjeAlf <>
2024-02-19 08:31:13 +0100

    Neue Datei, die alle selbstdefinierten Funktionen enthält

---
+++ Funktionen.py
@@ -0,0 +1,123 @@
+import random, sympy
+
+
+
+# Funktionen zur Darstellung von Zahlen
+
+def zzahl(p, q):
+    return random.choice([-1, 1]) * random.randint(p, q)
+
+def nzahl(p, q):
+    return random.randint(p, q)
+
+def vorz(k):
+    if k < 0:
+        return '-'
+    else:
+        return '+'
+
+def gzahl(k):
+    if k%1 == 0:
+        return latex(int(k))
+    else:
+        return latex(k)
+
+def vorz_gzahl(k):
+    if abs(k) == 1:
+        if k < 0:
+            return '-'
+        else:
+            return ''
+    if k%1 == 0:
+        return latex(int(k))
+    else:
+        return latex(k)
+
+def vorz_fakt(k):
+    if k < 0:
+        return -1
+    else:
+        return 1
+
+def vorz_str(k):
+    if k%1 == 0:
+        k = int(k)
+    if k < 0:
+        return latex(k)
+    else:
+        return f'+{latex(k)}'
+
+def vorz_str_minus(k):
+    if k%1 == 0:
+        k = int(k)
+    if k < 0:
+        return f'({latex(k)})'
+    else:
+        return latex(k)
+
+# Funktionen zur Optimierung von Ergebnissen mit True and False als Ausgabe
+
+def vektor_rational(vec,p,q=1000):
+    vec_p = [element*p for element in vec]
+    print(vec_p)
+    k = 0
+    i = 0
+    for element in vec_p:
+        if element % 1 == 0:
+            k += 1
+        if int(k/q) == 0:
+            i += 1
+    if k == 3 and i == 3:
+        return True
+    else:
+        return False
+
+# Funktionen zur linearen Algebra
+
+def punkt_vektor(p):
+    return np.array([zzahl(1,p), zzahl(1,p), zzahl(1,p)])
+
+def faktorliste(n, p=1,q=10):
+    return [zzahl(p, q) for _ in range(n)]  # mit dem _ kann man die Variable weglassen
+
+def vektor_runden(vec,p):
+    return [N(elements,p) for elements in vec]
+# Berechnung für die Aufgaben
+def vektor_ganzzahl(vec):
+    return np.array([int(element) if element % 1 == 0 else element for element in vec])
+
+def vektor_kürzen(vec, p = 50):
+    faktor = [x + 1 for x in range(p)]
+    list = np.array(vec)
+    i = 0
+    for element in vec:
+        k = 0
+        if list[i] % 1 == 0:
+            i += 1
+        else:
+            while (list[i] * faktor[k]) % 1 != 0 and k+1 < p:
+                k += 1
+            list = list * faktor[k]
+            i += 1
+    # print('erweitert: ' + str(list))
+    teiler = [x + 1 for x in range(int(max(list)/2))]
+    for zahl in teiler:
+        treffer = [1 for x in list if x % zahl == 0]
+        if sum(treffer) == len(vec):
+            list = list / zahl
+    # print('gekürzt: ' + str(list))
+    list = np.array([int(element) if element % 1 == 0 else element for element in list])
+    return np.array(list)
+
+def vektor_kollinear(vec1, vec2):
+    i = 0
+    lsg = []
+    for element in vec1:
+        lsg.append(element / vec2[i])
+        i += 1
+    # print(lsg)
+    for element in lsg:
+        # print(element / lsg[0])
+        if element / lsg[0] != 1:
+            return False
+    return True
