cfb01d4e0bc9c1b13e46527fbf2835dee83fcca4
HerrjeAlf <>
2024-02-26 12:36:05 +0100

    MA 13 neuen Test angefangen

---
+++ MA 13 Test - WR Grundbegriffe und Laplace.py
@@ -0,0 +1,164 @@
+import datetime
+import string
+import numpy as np
+import random, math
+from funktionen import *
+import matplotlib.pyplot as plt
+from numpy.linalg import solve as slv
+from pylatex import (Document, NoEscape, SmallText, LargeText, MediumText, NewPage, Tabular, Alignat, Figure,
+                     MultiColumn, MultiRow, Package)
+from pylatex.utils import bold
+from sympy import *
+from plotten import Graph
+# Definition der Funktionen
+
+a, b, c, d, e, f, g, h, x, y, z = symbols('a b c d e f g h x y z')
+liste_teilaufg = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+nr_aufgabe = 0
+
+def erstellen(Teil):
+    print(f'\033[38;2;100;141;229m\033[1m{Teil}\033[0m')
+    liste_bez = ['Aufgabe']
+    liste_punkte = ['Punkte']
+
+
+    def ergeignisse_ergebnisse(nr, teilaufg):
+        i = 0
+        aufgabe = [MediumText(bold('Aufgabe ' + str(nr) + ' \n\n'))]
+        loesung = [r' \mathbf{Lösung~Aufgabe~}' + str(nr) + r' \hspace{35em}']
+        grafiken_aufgaben = ['']
+        grafiken_loesung = ['']
+
+        if 'a' in teilaufg:
+            punkte_aufg = 5
+            liste_punkte.append(punkte_aufg)
+            liste_bez.append(str(nr) + '. ' + str(liste_teilaufg[i]) + ')')
+            grafiken_aufgaben.append(f'Aufgabe_{nr}{liste_teilaufg[i]}')
+            grafiken_loesung.append(f'Loesung_{nr}{liste_teilaufg[i]}')
+
+            aufgabe.append(str(liste_teilaufg[i]) + f')  \n\n')
+            loesung.append(str(liste_teilaufg[i]) + r') \quad \quad (3P) \\'
+                           r' \mathrm{insgesamt~' + str(punkte_aufg) + r'~Punkte} \\')
+            i += 1
+
+        return [aufgabe, loesung, grafiken_aufgaben, grafiken_loesung]
+
+
+    aufgaben = [ergeignisse_ergebnisse(1, ['a', 'b', 'c', 'd', 'e'])]
+
+    # erstellen der Tabelle zur Punkteübersicht
+    Punkte = (sum(liste_punkte[1:]))
+    liste_bez.append('Summe')
+    liste_punkte.append(str(Punkte))
+    anzahl_spalten = len(liste_punkte)
+    liste_ergebnis_z1 = ['erhaltene']
+    for p in range(anzahl_spalten - 1):
+        liste_ergebnis_z1.append('')
+    liste_ergebnis_z2 = ['Punkte']
+    for p in range(anzahl_spalten - 1):
+        liste_ergebnis_z2.append('')
+
+    spalten = '|'
+    for p in liste_punkte:
+        spalten += 'c|'
+
+    table2 = Tabular(spalten, row_height=1.2)
+    table2.add_hline()
+    table2.add_row((MultiColumn(anzahl_spalten, align='|c|', data='Punkteverteilung aller Aufgaben'),))
+    table2.add_hline()
+    table2.add_row(liste_bez)
+    table2.add_hline()
+    table2.add_row(liste_punkte)
+    table2.add_hline()
+    table2.add_row(liste_ergebnis_z1)
+    table2.add_row(liste_ergebnis_z2)
+    table2.add_hline()
+
+    # Angaben für den Test im pdf-Dokument
+    Datum = datetime.date.today().strftime('%d.%m.%Y')
+    Kurs = 'Grundkurs'
+    Fach = 'Mathematik'
+    Klasse = '13'
+    Lehrer = 'Herr Herrys'
+    Art = 'Test'
+    Titel = 'Lagebeziehung und Abstände von Ebenen'
+
+    # der Teil in dem die PDF-Datei erzeugt wird
+    def Hausaufgabenkontrolle():
+        geometry_options = {"tmargin": "0.2in", "lmargin": "1in", "bmargin": "0.4in", "rmargin": "0.7in"}
+        Aufgabe = Document(geometry_options=geometry_options)
+        Aufgabe.packages.append(Package('amsfonts'))  # fügt das Package 'amsfonts' hinzu, für das \mathbb{R} für reelle Zahlen
+        # erste Seite
+        table1 = Tabular('|c|c|c|c|c|c|', row_height=1.2)
+        table1.add_row((MultiColumn(6, align='c', data=MediumText(bold('Torhorst - Gesamtschule'))),))
+        table1.add_row((MultiColumn(6, align='c', data=SmallText(bold('mit gymnasialer Oberstufe'))),))
+        table1.add_hline()
+        table1.add_row('Klasse:', 'Fach:', 'Niveau:', 'Lehrkraft:', 'Datum:', 'Art:')
+        table1.add_hline()
+        table1.add_row(Klasse, Fach, Kurs, Lehrer, Datum, Art)
+        table1.add_hline()
+        Aufgabe.append(table1)
+        Aufgabe.append(' \n\n\n\n')
+        Aufgabe.append(LargeText(bold(f' {Titel} \n\n')))
+        for aufgabe in aufgaben:
+            k = 0
+            for elements in aufgabe[0]:
+                if '~' in elements:
+                    with Aufgabe.create(Alignat(aligns=1, numbering=False, escape=False)) as agn:
+                        agn.append(elements)
+                elif 'Abbildung' in elements:
+                    Aufgabe.append(elements)
+                    with Aufgabe.create(Figure(position='h!')) as graph:
+                        graph.add_image(aufgabe[2][k], width='200px')
+                else:
+                    Aufgabe.append(elements)
+                k += 1
+
+        Aufgabe.append('\n\n')
+        Aufgabe.append(table2)
+
+        Aufgabe.append(NewPage())
+        Aufgabe.append(LargeText(bold(Teil + ' - bearbeitet von:')))
+
+        Aufgabe.generate_pdf(f'Ma {Klasse} - {Art} {Teil}', clean_tex=true)
+        print('\033[38;2;0;220;120m\033[1mKontrolle erstellt\033[0m')
+
+    # Erwartungshorizont
+    def Erwartungshorizont():
+        geometry_options = {"tmargin": "0.4in", "lmargin": "1in", "bmargin": "1in", "rmargin": "1in"}
+        Loesung = Document(geometry_options=geometry_options)
+        Loesung.packages.append(Package('amsfonts'))
+        Loesung.append(LargeText(bold(f'Loesung für {Art} {Teil} \n\n {Titel} \n\n')))
+
+        for loesung in aufgaben:
+            k = 0
+            for elements in loesung[1]:
+                if '~' in elements:
+                    with Loesung.create(Alignat(aligns=2, numbering=False, escape=False)) as agn:
+                        agn.append(elements)
+                elif 'Abbildung' in elements:
+                    Loesung.append(elements)
+                    with Loesung.create(Figure(position='h!')) as graph:
+                        graph.add_image(loesung[3][k], width='200px')
+                else:
+                    Loesung.append(elements)
+                k += 1
+        Loesung.append(MediumText(bold(f'insgesamt {Punkte} Punkte')))
+
+        Loesung.generate_pdf(f'Ma {Klasse} - {Art} {Teil} - Lsg', clean_tex=true)
+        print('\033[38;2;0;220;120m\033[1mErwartungshorizont erstellt\033[0m')
+
+    # Druck der Seiten
+    Hausaufgabenkontrolle()
+    Erwartungshorizont()
+
+
+anzahl_Arbeiten = 2
+probe = False
+alphabet = string.ascii_uppercase
+for teil_id in range(anzahl_Arbeiten):
+    if probe:
+        erstellen('Probe {:02d}'.format(teil_id + 1))
+    else:
+        erstellen(f'Gr. {alphabet[teil_id]}')
+    print()  # Abstand zwischen den Arbeiten (im Terminal)
