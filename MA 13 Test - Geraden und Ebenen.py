097f830f3b14a20fef65fba68477f511170794fa
HerrjeAlf <>
2024-02-27 17:00:57 +0100

    neuen test gedruckt

--- MA 13 Test - Geraden und Ebenen.py
+++ MA 13 Test - Geraden und Ebenen.py
@@ -729,8 +729,8 @@ def erstellen(Teil):
     Hausaufgabenkontrolle()
     Erwartungshorizont()
 
-anzahl_Arbeiten = 3
-probe = True
+anzahl_Arbeiten = 1
+probe = False
 alphabet = string.ascii_uppercase
 for teil_id in range(anzahl_Arbeiten):
     if probe:
