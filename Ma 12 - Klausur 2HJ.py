3cb90aa1e56b2cf8e9b009475970ad4530f4181a
HerrjeAlf <>
2024-04-22 09:18:04 +0200

    leerzeile rausgenommen

--- Ma 12 - Klausur 2HJ.py
+++ Ma 12 - Klausur 2HJ.py
@@ -27,8 +27,6 @@ for element in aufgaben_hmft_seite3:
     liste_bez_hmft.extend(element[5])
     liste_punkte_hmft.extend(element[4])
 
-
-
 liste_seiten_hmft = [seite(aufgaben_hmft_seite2), seite(aufgaben_hmft_seite3)]
 # z.b. liste_seiten = [seite(aufgaben_seite1), seite(aufgaben_seite2)]
 
