36d3c1f49730be88500264d27c6eba4bb196f3e2
alfgorowing <a.herrys@outlook.de>
2024-09-24 17:31:51 +0200

    neuen Ordner temp für temporäre Dateien eingeführt

---
+++ temp/temp.py
@@ -0,0 +1,157 @@
+import random
+
+from sympy import *
+from skripte.funktionen import *
+
+a, b, c, d, e, f, g, h, x, y, z = symbols('a b c d e f g h x y z')
+# b = list(range(1,4))
+# print(b)
+
+# i = 0
+# for m in range(1,7):
+#     for n in range(1,7):
+#         if m + n > 9:
+#             print ('m: ' + str(m) + ' und n: ' + str(n) + ' und m+n:' + str(m+n))
+#             i += 1
+# print(i)
+
+# a = [[1,2],[2,3]]
+
+# wert = 123.3
+# wert_neu = str(wert).replace('.', ',')
+
+# b = zzahl(2,5)
+# print(b)
+# extrema_xwert = zzahl(1,4)
+# nst= vorz_fakt(extrema_xwert) * (abs(extrema_xwert) + nzahl(1,3))
+# c = -1*nst*b
+# print(c)
+# fkt_v = exp(a * x)*(b*x + c)
+# fkt_a1 = diff(fkt_v, x)
+# gleichung2 = Eq(fkt_a1.subs(x, extrema_xwert), 0)
+# lsg = solve(fkt_a1.subs(x, extrema_xwert), a)
+# fkt = exp(lsg[0]*x)*(b*x+c)
+# fkt_1 = diff(fkt,x)
+# fkt_2 = diff(fkt, x, 2)
+# fkt_3 = diff(fkt,x,3)
+# fkt_1_str_zw = (gzahl(b) + r' \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'} + (' + vorz_v_aussen(b, 'x')
+#                 + vorz_str(c) + r') \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'} \cdot '
+#                 + gzahl_klammer(lsg[0]))
+# fkt_1_str = ('(' + vorz_v_aussen(lsg[0] * b, 'x') + vorz_str(lsg[0] * c + b)
+#              + r') \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'}')
+# fkt_2_str_zw = (gzahl(lsg[0] * b) + r' \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'} + '
+#                 + '(' + vorz_v_aussen(lsg[0] * b, 'x') + vorz_str(lsg[0] * c + b)
+#                 + r') \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'} \cdot '
+#                 + gzahl_klammer(lsg[0]))
+# fkt_2_str = ('(' + vorz_v_aussen(lsg[0] ** 2 * b, 'x') + vorz_str(lsg[0] ** 2 * c + 2 * lsg[0] * b)
+#              + r') \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'}')
+# fkt_3_str_zw = (gzahl(lsg[0] ** 2 * b) + r' \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'} + '
+#                 + '(' + vorz_v_aussen(lsg[0] ** 2 * b, 'x') + vorz_str(lsg[0] ** 2 * c + 2 * lsg[0] * b)
+#                 + r') \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'} \cdot '
+#                 + gzahl_klammer(lsg[0]))
+# fkt_3_str = ('(' + vorz_v_aussen(lsg[0] ** 3 * b, 'x') + vorz_str(lsg[0] ** 3 * c + 3 * lsg[0] ** 2 * b)
+#              + r') \cdot e^{' + vorz_v_aussen(lsg[0], 'x') + r'}')
+# ywerte = [(element, fkt.subs(x, element)) for element in range(-20, 20)]
+# k = 0
+# wertebereich = []
+# for zahl in range(len(ywerte)-1):
+#     if abs(ywerte[k][1]) < 6 and abs(ywerte[k][1] - ywerte[k + 1][1]) > 0.5:
+#         wertebereich.append(ywerte[k][0])
+#     k += 1
+# print(wertebereich)
+# xmin = wertebereich[0]
+# xmax = wertebereich[-1]
+# plot(fkt, (x,xmin,xmax))
+# print(fkt)
+# print(lsg)
+# print('x_E: ' + str(extrema_xwert))
+# # print('y_E: ' + str(extrema_ywert))
+# print('nst: ' + str(nst))
+# print(fkt_1_str_zw)
+# print(fkt_1_str)
+# print(latex(fkt_1))
+# print(fkt_2_str_zw)
+# print(fkt_2_str)
+# print(latex(fkt_2))
+# print(fkt_2_str_zw)
+# print(fkt_2_str)
+# print(latex(fkt_3))
+
+#extrema_xwert = zzahl(1, 3)
+#extrema_ywert = zzahl(1, 3)
+#if extrema_xwert > 0:
+#    y_vers = -1 * nzahl(1, 3)
+#else:
+#    y_vers = nzahl(1, 3)
+#fkt_v = exp(b * x + 2) * a * x ** 2
+#fkt_a1 = diff(fkt_v, x)
+#gleichung1 = Eq(fkt_v.subs(x, extrema_xwert), extrema_ywert)
+#gleichung2 = Eq(fkt_a1.subs(x, extrema_xwert), 0)
+#lsg = solve((gleichung1, gleichung2), (a, b))
+#lsg_a = lsg[0][0]
+#lsg_b = lsg[0][1]
+# print(lsg)
+#fkt = exp(lsg_b * x + 2) * lsg_a * x ** 2 + y_vers
+#fkt_str = (vorz_v_aussen(lsg_a, 'x^2') + r' \cdot e^{' + vorz_v_aussen(lsg_b, 'x+2') + '}'
+#           + vorz_str(y_vers))
+
+# Werte für Angaben zum Zeichnen des Graphen
+#ywerte = [(element, fkt.subs(x, element)) for element in range(-5, 6)]
+#wertebereich = [element[0] for element in ywerte if abs(element[1]) < 6]
+#xmin = wertebereich[0]
+#xmax = wertebereich[-1]
+#plot(fkt,(x,xmin,xmax))
+#print(lsg)
+
+def aufg_lsg(exponenten, anz_bas):
+    ar_ausw_bas = random_selection(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'x', 'y', 'z'], anz_bas, False)
+    ausw_bas = [element for element in ar_ausw_bas]
+    # print(ausw_bas)
+    ausw_bas.sort()
+    bas_exp = [[random.choice(ausw_bas), element] for element in exponenten]
+    # print(ausw_bas)
+    # print(bas_exp)
+    aufg = ''
+    m = 1
+    for element in bas_exp:
+        # print(element)
+        if m != len(bas_exp):
+            # print(element[0])
+            # print(element[1])
+            aufg = aufg + element[0] + '^{' + gzahl(element[1]) + r'}~ \cdot ~'
+        else:
+            aufg = aufg + element[0] + '^{' + gzahl(element[1])
+        m += 1
+    lsg = aufg + '~=~'
+    exp_sort = []
+    for basis in ausw_bas:
+        exp_der_basis = []
+        for element in bas_exp:
+            if basis == element[0]:
+                exp_der_basis.append(element[1])
+        lsg = lsg + basis + '^{' + gzahl(exp_der_basis[0])
+        k = 1
+        for zahl in range(len(exp_der_basis) - 1):
+            lsg = lsg + vorz_str(exp_der_basis[k])
+            k += 1
+        lsg = lsg + '}'
+        if basis != ausw_bas[-1]:
+            lsg = lsg + r'~ \cdot ~'
+        exp_sort.append(exp_der_basis)
+
+    lsg = lsg + '~=~'
+    k = 0
+    print(exp_sort)
+    for basis in ausw_bas:
+        if basis != ausw_bas[-1]:
+            lsg = lsg + basis + '^{' + gzahl(sum(exp_sort[k])) + r'} \cdot '
+        else:
+            lsg = lsg + basis + '^{' + gzahl(sum(exp_sort[k])) + '}'
+        k += 1
+    return aufg, lsg
+
+aufg, lsg = aufg_lsg([2,3,4,5],2)
+
+print(aufg)
+print(lsg)
+
---
+++ temp/testen.py
@@ -0,0 +1,33 @@
+from Aufgaben import *
+from skripte.erstellen import *
+
+# Angaben für den Test im pdf-Dokument
+schule = 'Torhorst - Gesamtschule'
+schulart = 'mit gymnasialer Oberstufe'
+Kurs = 'Leistungskurs'
+Fach = 'Mathematik'
+Klasse = '12'
+Lehrer = 'Herr Herrys'
+Art = 'Testen'
+Titel = 'Hier wird getestet'
+datum_delta = 1  # in Tagen (0 ist Heute und 1 ist Morgen, 2 Übermorgen, usw.)
+anzahl = 1 # wie viele verschiedenen Tests sollen erzeugt werden
+probe = False   # True: Probe 01, 02 usw. oder Gr. A, Gr. B usw
+
+liste_punkte = ['Punkte']
+liste_bez = ['Aufgabe']
+
+for ziffer in range(anzahl):
+    # Hier die Aufgaben in der Form [[aufgabe1(), aufgabe2()],[aufgabe3(), aufgabe4()]] eintragen
+    Aufgaben = [[rechnen_mit_vektoren(2)]]
+
+    # hier werden aus der Liste der Aufgaben dieTest erzeugt
+    liste_seiten = []
+    for element in Aufgaben:
+        for aufgabe in element:
+            liste_bez.extend(aufgabe[5])
+            liste_punkte.extend(aufgabe[4])
+        liste_seiten.append(seite(element)) # z.b. liste_seiten = [seite(aufgaben_seite1), seite(aufgaben_seite2)]
+
+    angaben = [schule, schulart, Kurs, Fach, Klasse, Lehrer, Art, Titel, datum_delta, liste_bez, liste_punkte]
+    test_erzeugen(liste_seiten, angaben, ziffer, probe)
