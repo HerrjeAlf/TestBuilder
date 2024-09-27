ce1367b426b81cd8908c4d10191007d1e0fc5222
alfgorowing <a.herrys@outlook.de>
2024-09-27 15:07:36 +0200

    Test erzeugt

--- temp/MA 13 Test Punkte und Vektoren.py
+++ temp/MA 13 Test Punkte und Vektoren.py
@@ -15,8 +15,8 @@ Lehrer = 'Herr Herrys'
 Art = 'Test'
 Titel = 'Rechnen mit Vektoren'
 datum_delta = 1  # in Tagen (0 ist Heute und 1 ist Morgen, 2 Übermorgen, usw.)
-anzahl = 2 # wie viele verschiedenen Tests sollen erzeugt werden
-probe = False # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
+anzahl = 1 # wie viele verschiedenen Tests sollen erzeugt werden
+probe = True # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
 
 liste_punkte = ['Punkte']
 liste_bez = ['Aufgabe']
