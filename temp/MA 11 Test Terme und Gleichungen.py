91c78d443b7597935b15e39895da3cf8b737f730
alfgorowing <a.herrys@outlook.de>
2024-11-04 21:20:49 +0100

    Arbeitsblatt zum Wiederholen und üben erstellt

--- temp/MA 11 Test Terme und Gleichungen.py
+++ temp/MA 11 Test Terme und Gleichungen.py
@@ -23,8 +23,8 @@ liste_bez = ['Aufgabe']
 
 for i in range(anzahl):
     # Hier die Aufgaben in der Form [[aufgabe1(), aufgabe2()],[aufgabe3(), aufgabe4()], usw.] eintragen
-    Aufgaben = [[wiss_schreibweise(1),
-                 einheiten_umrechnen(2),
+    Aufgaben = [[wiss_schreibweise(1, anzahl=12),
+                 einheiten_umrechnen(2, anzahl=8),
                  terme_addieren(3)]]
     # hier werden aus der Liste der Aufgaben dieTest erzeugt
     liste_seiten = []
