1d7f85d950c5a0520c5a2a9e57a7a653f7e8eabe
alfgorowing <a.herrys@outlook.de>
2024-12-29 07:22:46 +0100

    Fehler korrigiert

--- Aufgaben/Oberstufe_Algebra.py
+++ Aufgaben/Oberstufe_Algebra.py
@@ -2397,6 +2397,34 @@ def ebenenschar_buendel(nr, teilaufg=['a', 'b', 'c', 'd'], BE=[]):
         liste_punkte.append(pkt)
         i += 1
 
+    if 'e' in teilaufg:
+        # die SuS sollen die Schnittebene zweier Ebenen der Schar bestimmen und nachweisen, dass diese in allen Ebenen liegt
+        var1 = zzahl(1,2)
+        var2 = var1 + 2
+        liste_bez.append(f'{str(nr)}.{str(liste_teilaufg[i])})')
+
+        aufgabe.append(NoEscape(str(liste_teilaufg[i]) + f') Berechnen Sie die Schnittgerade der Ebenen '
+                       + f'für $ E_{latex(var1)} $ und $ E_{latex(var2)} $ ob es eine Ebene der Ebenenschar gibt, '
+                       + f'die parallel zur Geraden h ist. \n\n')
+        loesung.append(str(liste_teilaufg[i]) + r') \quad \mathrm{Skalarprodukt~des~Richtungsvektor~von~h~und~dem'
+                       + r'~Normalenvektor~von~E_a~aufstellen~und~a~berechnen} \\'
+                       + r'0~=~ \begin{pmatrix} ' + binom(nx, aex, str2='a') + r' \\'
+                       + binom(ny, aey, str2='a') + r' \\' + binom(nz, aez, str2='a') + r' \\ \end{pmatrix} '
+                       + r' \cdot \begin{pmatrix} ' + gzahl(h_vx) + r' \\' + gzahl(h_vy) + r' \\' + gzahl(h_vz) + r' \\'
+                       + r' \end{pmatrix} ~=~' + binom_aussen(nx, aex, str2='a') + r' \cdot '
+                       + gzahl_klammer(h_vx) + binom_innen(ny, aey, str2='a', ) + r' \cdot ' + gzahl_klammer(h_vy)
+                       + binom_innen(nz, aez, str2='a') + r' \cdot ' + gzahl_klammer(h_vz) + r' \quad (1BE) \\'
+                       + '0~=~' + gzahl(nx * h_vx) + vorz_str(ny * h_vy) + vorz_str(nz * h_vz)
+                       + vorz_v_innen(aex * h_vx, 'a') + vorz_v_innen(aey * h_vy, 'a')
+                       + vorz_v_innen(aez * h_vz, 'a') + r'~=~' + gzahl(nx * h_vx + ny * h_vy + nz * h_vz)
+                       + vorz_v_innen(aex * h_vx + aey * h_vy + aez * h_vz, 'a')
+                       + r' \quad \vert ' + vorz_str(-1 * (nx * h_vx + ny * h_vy + nz * h_vz)) + r' \quad \vert \div '
+                       + gzahl_klammer(aex * h_vx + aey * h_vy + aez * h_vz) + r' \quad \to \quad a~=~' + gzahl(h_var)
+                       + r' \quad (2BE) \\ E_a:' + vorz_v_aussen(nx + aex * erg, 'x')
+                       + vorz_v_innen(ny + aey * erg, 'y') + vorz_v_innen(nz + aez * erg, 'z') + r' \quad (1BE)')
+        liste_punkte.append(pkt)
+        i += 1
+
     if BE != []:
         if len(BE) != len(teilaufg):
             print(f'Die Anzahl der gegebenen BE ({len(BE)}) stimmt nicht mit der Anzahl der Teilaufgaben ({len(teilaufg)}) überein. Es wird die ursprüngliche Punkteverteilung übernommen.')
