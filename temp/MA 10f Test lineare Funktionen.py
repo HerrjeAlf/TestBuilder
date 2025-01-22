1f6cde44e1365c3e4169f462b1aed772bafe3e83
alfgorowing <a.herrys@outlook.de>
2025-01-22 14:43:42 +0100

    Test erstellt

--- temp/MA 10f Test lineare Funktionen.py
+++ temp/MA 10f Test lineare Funktionen.py
@@ -12,11 +12,11 @@ Kurs = ('EK und GK')
 Fach = 'Mathematik'
 Klasse = '10f'
 Lehrer = 'Herr Herrys'
-Art = 'HAK 08'
+Art = 'HAK 06'
 Titel = 'Wiederholung lineare Funktionen'
 datum_delta = 1  # in Tagen (0 ist Heute und 1 ist Morgen, 2 Übermorgen, usw.)
-anzahl = 1 # wie viele verschiedenen Tests sollen erzeugt werden
-probe = True # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
+anzahl = 2 # wie viele verschiedenen Tests sollen erzeugt werden
+probe = False # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
 
 liste_punkte = ['Punkte']
 liste_bez = ['Aufgabe']
