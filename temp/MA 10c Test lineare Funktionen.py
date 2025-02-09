9985aeea7b7950ce65521c5d72ad30f628c393cf
HerrjeAlf <113554034+HerrjeAlf@users.noreply.github.com>
2025-02-09 17:30:39 +0100

    Teilaufgaben bei Parabeln erstellt

--- temp/MA 10c Test lineare Funktionen.py
+++ temp/MA 10c Test lineare Funktionen.py
@@ -15,15 +15,15 @@ Lehrer = 'Herr Herrys'
 Art = 'HAK 07'
 Titel = 'Wiederholung lineare Funktionen'
 datum_delta = 1  # in Tagen (0 ist Heute und 1 ist Morgen, 2 Übermorgen, usw.)
-anzahl = 1 # wie viele verschiedenen Tests sollen erzeugt werden
-probe = True # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
+anzahl = 2 # wie viele verschiedenen Tests sollen erzeugt werden
+probe = False # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
 
 liste_punkte = ['Punkte']
 liste_bez = ['Aufgabe']
 
 for i in range(anzahl):
     # Hier die Aufgaben in der Form [[aufgabe1(), aufgabe2()],[aufgabe3(), aufgabe4()], usw.] eintragen
-    Aufgaben = [[lineare_funktionen(1, ['a', 'c'], anz_einf=3, anz_pkt=1)]]
+    Aufgaben = [[lineare_funktionen(1, ['a', 'c'], anz_einf=3, anz_pkt=0)]]
     # hier werden aus der Liste der Aufgaben dieTest erzeugt
     liste_seiten = []
     for element in Aufgaben:
