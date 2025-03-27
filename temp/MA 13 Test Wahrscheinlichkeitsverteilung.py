9acdc2b59187daf24154faaef9b747452dd77755
HerrjeAlf <113554034+HerrjeAlf@users.noreply.github.com>
2025-03-27 21:57:05 +0100

    neuen Test erstellt

--- temp/MA 13 Test Wahrscheinlichkeitsverteilung.py
+++ temp/MA 13 Test Wahrscheinlichkeitsverteilung.py
@@ -19,7 +19,7 @@ Art = 'HAK 06'
 Titel = 'Binomialverteilung'
 datum_delta = 1  # Wann wird der Test geschrieben (in Tagen - 0 ist Heute, 1 ist Morgen, 2 Übermorgen, usw.)
 anzahl = 2 # wie viele verschiedenen Tests sollen erzeugt werden
-probe = True # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
+probe = False # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
 clean_tex = True # Hier kann mit True oder False festgelegt werden, ob die Latex-Datei gelöscht werden soll
 
 clean_tex = True if clean_tex not in [True, False] else clean_tex
