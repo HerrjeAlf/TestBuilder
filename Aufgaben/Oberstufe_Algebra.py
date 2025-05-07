0ed6b03de8c9d934657573785859b0eaa0f6d335
HerrjeAlf <113554034+HerrjeAlf@users.noreply.github.com>
2025-05-07 21:18:52 +0200

    Fehler nach Nickolas behoben

--- Aufgaben/Oberstufe_Algebra.py
+++ Aufgaben/Oberstufe_Algebra.py
@@ -1388,7 +1388,7 @@ def geraden_lagebeziehung(nr, teilaufg=['a', 'b', 'c', 'd', 'e', 'f'], lagebezie
                        + r' \quad \mathrm{und} \quad \vert \overrightarrow{v} \vert ~=~ \sqrt{ ('
                        + str(px) + ')^2 ~+~(' + str(py) + ')^2 ~+~(' + str(pz)
                        + ')^2} ~=~ ' + gzahl(N(l_p,3)) + r' \quad (2BE) \\'
-                       + r' \gamma ~=~ cos^{-1} \left( \frac{' + gzahl(abs(N(sp_vp,3))) + '}{'
+                       + r' \gamma ~=~ cos^{-1} \left( \frac{' + gzahl(N(sp_vp,3)) + '}{'
                        + gzahl(N(l_v,3)) + r' \cdot ' + gzahl(N(l_pw,3))
                        + r'} \right) ~=~' + gzahl(erg) + text
                        + r' \quad (2BE) \\ \mathrm{insgesamt~' + str(punkte_aufg) + r'~BE} \\')
