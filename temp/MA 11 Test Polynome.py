a95e6354f86a67ab597a0a65ca60212e75ab0a10
alfgorowing <a.herrys@outlook.de>
2025-05-14 07:47:27 +0200

    beschriftung neue Klasse eingefügt

--- temp/MA 11 Test Polynome.py
+++ temp/MA 11 Test Polynome.py
@@ -15,7 +15,7 @@ Lehrer = 'Herr Herrys'
 Art = 'Test'
 Titel = 'Polynome'
 datum_delta = 1  # in Tagen (0 ist Heute und 1 ist Morgen, 2 Übermorgen, usw.)
-anzahl = 2 # wie viele verschiedenen Tests sollen erzeugt werden
+anzahl = 1 # wie viele verschiedenen Tests sollen erzeugt werden
 probe = [True, False][1] # True: Probe 01, 02 usw. oder False: Gr. A, Gr. B usw
 
 liste_punkte = ['Punkte']
